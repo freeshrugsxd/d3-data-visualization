e5fef1b44bda76fc4feb583606c9aebcdcb9e9f7
silvio <>
2019-01-28 10:52:02 +0100

    code cleanup and semicolon placement

--- pie-chart/d3_piechart.js
+++ pie-chart/d3_piechart.js
@@ -1,13 +1,13 @@
 function draw_pie(configs, side) {
 
-    let config  = side == 'l' ? configs.left : configs.right
+    let config  = side === 'l' ? configs.left : configs.right,
         divId   = config.div_id,
         chartId = config.chart_id,
         margin  = config.margin,
         objArr  = [],
-        w       = $(divId).width()
+        w       = $(divId).width();
 
-    $(divId).html("") // delete all content of div container
+    $(divId).html('') // delete all content of div container
 
     // write json objects into array
     Object.keys(config.data).forEach(function(key) {
@@ -19,32 +19,32 @@ function draw_pie(configs, side) {
         .range(['#FF3112', '#1231FF']);
 
     let innerRadius = 0,
-        outerRadius = (w - margin.left - margin.right) / 2
+        outerRadius = (w - margin.left - margin.right) / 2;
 
     let arc = d3.svg.arc()
         .innerRadius(innerRadius)
         .outerRadius(outerRadius);
 
     // create svg area
-    let h = 2 * outerRadius + margin.top + margin.bottom
+    let h = 2 * outerRadius + margin.top + margin.bottom;
     let svg = d3.selectAll(divId)
         .append('svg')
         .attr({
                 id : chartId,
              width : w,
             height : h
-        })
+        });
 
     // x and y coords of the pie chart centre
     let cx = outerRadius + margin.left,
-        cy = outerRadius + margin.top
+        cy = outerRadius + margin.top;
 
     let group = svg.append('g')
         .attr('transform', 'translate(' + cx + ',' + cy + ')')
-        .attr('class', 'pieChart')
+        .attr('class', 'pieChart');
 
     let pie = d3.layout.pie()
-        .value(function(d) {
+        .value(function(d){
             return d3.sum(d3.values(d))
         });
 
@@ -71,7 +71,7 @@ function draw_pie(configs, side) {
                 configs.right.data.push({key : arcData[key]})
             })
             draw_pie(configs, 'r')
-        })
+        });
 
     let text = group.selectAll('text')
         .data(pie(objArr))
--- pie-chart/main.js
+++ pie-chart/main.js
@@ -1,43 +1,41 @@
 let pie_area = (function(){
     d3.json('pie-chart/pie_data.json', init)
 
-    let template = {
+    let configs  = {},
+        template = {
          ref_width : 350,
         ref_height : 350,
          min_width : 200,
         min_height : 200,
             div_id : '#leftDiv',
           chart_id : '#leftChart',
-            margin : {    top : 40,
-                        right : 40,
-                       bottom : 40,
-                         left : 40 },
+            margin : {    top : 25,
+                        right : 25,
+                       bottom : 25,
+                         left : 25 },
               data : [],
-    }
-    let config = {}
-    config.left = template
-    config.right = Object.assign({}, config.left)
-    config.right.div_id = '#rightDiv'
-    config.right.chart_id = '#rightChart'
+    };
 
+    configs.left  = template;
+    configs.right = Object.assign({}, configs.left);
+    jQuery.extend(configs.right, { div_id : '#rightDiv', chart_id : '#rightChart' });
 
     function init(json) {
-        config.left.data = json.data;
-        redraw();
+        configs.left.data = json.data;
+        redraw()
     }
 
     function redraw() {
-        left_chart = draw_pie(config, 'l');
-        right_chart = draw_pie(config, 'r');
+        left_chart = draw_pie(configs, 'l')
+        right_chart = draw_pie(configs, 'r')
     }
 
-    return {
-        render : redraw
-    }
+    return { render : redraw }
 
-})();
+})()
+
 
 let resize_windows = function(){
-    window.dispatchEvent(new Event('resize'));
+    window.dispatchEvent(new Event('resize'))
 }
-window.addEventListener('resize', pie_area.render);
+window.addEventListener('resize', pie_area.render)
