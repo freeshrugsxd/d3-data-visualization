a15b508144cae1d6a3be96bcea1f62aea1eafec5
silvio <>
2019-01-30 11:10:37 +0100

    added transition to mouseover, cleaned some code

--- pie-chart/d3_piechart.js
+++ pie-chart/d3_piechart.js
@@ -18,51 +18,60 @@ function draw_pie(configs, side) {
         .domain([0, objArr.length])
         .range(['#FF3112', '#1231FF']);
 
-    let innerRadius = 0,
-        outerRadius = (w - margin.left - margin.right) / 2;
+    let outerRadius = (w - margin.left - margin.right) / 2,
+        innerRadius = 0,
+        h = 2 * outerRadius + margin.top + margin.bottom;
+
+    let pie = d3.layout.pie()
+        .value(function(d){ return d3.sum(d3.values(d)) });
 
     let arc = d3.svg.arc()
         .innerRadius(innerRadius)
         .outerRadius(outerRadius);
 
-    // create svg area
-    let h = 2 * outerRadius + margin.top + margin.bottom;
-    let svg = d3.selectAll(divId)
-        .append('svg')
-        .attr({
-                id : chartId,
-             width : w,
-            height : h
-        });
+    // arc generator with slightly bigger radius
+    // for mouseover transition
+    let arcMouseOver = d3.svg.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(outerRadius * 1.05)
 
     // x and y coords of the pie chart centre
     let cx = outerRadius + margin.left,
-        cy = outerRadius + margin.top;
+        cy = outerRadius + margin.top,
 
-    let group = svg.append('g')
+    // create svg area
+    let svg = d3.selectAll(divId)
+        .append('svg')
+        .attr({id : chartId, width : w, height : h})
+        .append('g')
         .attr('transform', 'translate(' + cx + ',' + cy + ')')
-        .attr('class', 'pieChart');
 
-    let pie = d3.layout.pie()
-        .value(function(d){
-            return d3.sum(d3.values(d))
-        });
-
-    let arcs = group.selectAll('path')
+    // append group for each datum in objArr
+    let arcs = svg.selectAll('.arc')
         .data(pie(objArr))
         .enter()
-        .append('path')
+        .append('g')
+        .attr('class', 'arc');
+
+    arcs.append('path')
         .attr('d', arc)
         .attr('fill', function(d, i) { return color(i) })
-
-        .on('mouseover', function() {
-            d3.select(this).attr('fill-opacity', .9)
+        .style('stroke', 'white')
+        .on('mouseover', function(){
+            d3.select(this)
+                .transition()
+                .duration(125)
+                .ease('in')
+                .attr('d', arcMouseOver);
         })
-
         .on('mouseout', function() {
-            d3.select(this).attr('fill-opacity', 1)
+            d3.select(this)
+                .transition()
+                .ease('bounce')
+                .delay(100)
+                .duration(500)
+                .attr('d', arc);
         })
-
         .on('click', function() {
             let arcData = this.__data__.data
             configs.right.data = []
@@ -73,12 +82,11 @@ function draw_pie(configs, side) {
             draw_pie(configs, 'r')
         });
 
-    let text = group.selectAll('text')
-        .data(pie(objArr))
-        .enter()
-        .append('text')
-        .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')' })
-        .attr('text-anchor', 'middle')
+    arcs.append('text')
+        .attr('transform', function(d) {
+            return 'translate(' + arc.centroid(d) + ')'
+        })
         .text(function(d) { return d.value })
-        .style('font-size', '18px');
+        .attr('text-anchor', 'middle')
+        .style('font-size', '20px');
 }
