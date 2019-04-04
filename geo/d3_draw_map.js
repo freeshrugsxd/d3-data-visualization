8c5d78708fc435a7c3b6c267a00b327771de42c2
silvio <silvio.bentzien@gmx.de>
2019-04-04 17:10:06 +0200

    worked out a better algorithm for marker clustering; still WIP

--- geo/d3_draw_map.js
+++ geo/d3_draw_map.js
@@ -10,12 +10,13 @@ function draw_map(config) {
     showGrid = config.graticule,
     rotation = config.rotation || [0, 0],
     n = config.features.length
-    config.scale = 1;
+    config.scale = 1,
+    pi = Math.PI;
 
   $(divClassSel).html('')
 
   let zoom = d3.behavior.zoom()
-    .scaleExtent([1, 15])
+    .scaleExtent([1, 25])
     .on('zoom', panAndZoom)
 
   let svg = d3.select(divClassSel)
@@ -26,8 +27,8 @@ function draw_map(config) {
   let mainGrp = svg.append('g')
 
   const projections = {
-    mercator : d3.geo.mercator().scale(width / 3 / Math.PI),
-    equirect : d3.geo.equirectangular().scale(width /2 / Math.PI),
+    mercator : d3.geo.mercator().scale(width / 3 / pi),
+    equirect : d3.geo.equirectangular().scale(width / 2 / pi),
     globe    : d3.geo.orthographic()
                  .clipAngle(90)
                  .scale(200)
@@ -55,6 +56,7 @@ function draw_map(config) {
       });
   }
 
+
   // add country boundaries
   let bounds = mainGrp.selectAll(boundClassSel)
     .data(config.features)
@@ -64,29 +66,49 @@ function draw_map(config) {
         .attr('d', path)
         .style({ fill: 'silver', stroke: 'white' });
 
-  config.features.map( function(d) {
-    d.data = {}
-    d.data.count = Math.random() / Math.random() + Math.random() + 25
-  })
+  let max = 0
+  for (let i in config.features) {
+    max = Math.max(max, config.features[i].properties.pop_est)
+  }
+
+  let maxRad = 25
+
+  let rad = d3.scale.pow()
+    .range([2, maxRad])
+    .domain([0, max])
+
+  let radii = Array(n),
+      nodes = Array(n);
+
+  let quadtree = d3.geom.quadtree()
+    .x(d => d.x)
+    .y(d => d.y)
+
 
   function update(config) {
 
-    nodes = config.features
+    // add random data to features
+    config.features.map( function(d, i) {
+      nodes[i] = {}
+      nodes[i].index = i
+      nodes[i].count = 1
 
-    nodes.map( function(d, i) {
-      // get centroids for every feature in the dataset
       xy = path.centroid(d)
-      d.data.x = xy[0]
-      d.data.y = xy[1]
-      d.data.index = i
+
+      nodes[i].x = xy[0]
+      nodes[i].y = xy[1]
+      nodes[i].r = nodes[i].count * 10 / Math.sqrt(config.scale)
+      nodes[i].a = pi * [nodes[i].r * nodes[i].r]
+
+      radii[i] = nodes[i].r
     })
 
-    let quadtree = d3.geom.quadtree()
-      .x(d => d.data.x)
-      .y(d => d.data.y)
+    // for (let i in nodes) {
+    //   nodes[i].r = rad(nodes[i].r) / config.scale
+    // }
 
     qtree = quadtree(nodes)
-
+    console.log('qtree', qtree)
 
     // draw qtree:
     // mainGrp.selectAll('rect').remove()
@@ -102,52 +124,99 @@ function draw_map(config) {
     //       .style('opacity', 0.6)
     //   })
 
-    let clusterPoints = [],
-        clusterRange  = parseInt(width / 20 / config.scale);
+    for (let i in nodes) {
+      n1 = nodes[i]
+      cx = n1.x
+      cy = n1.y
+      ra = radii[n1.index]
 
-    for (let x = 0; x <= width; x += clusterRange) {
-      for (let y = 0; y <= height; y+= clusterRange) {
-        let searched  = search(qtree, x, y, x + clusterRange, y + clusterRange),
-          centerPoint = searched.reduce( function(coll, curr) {
-            return {x: coll.x + curr.x,
-                    y: coll.y + curr.y,
-               count : coll.count + curr.count}
-          }, {x: 0, y: 0, count: 0});
-
-        centerPoint.x = centerPoint.x / searched.length;
-        centerPoint.y = centerPoint.y / searched.length;
-
-        if (centerPoint != {x: 0, y: 0, count: 0}) {
-          clusterPoints.push(centerPoint);
-        }
-      }
+      search(qtree, cx, cy, ra)
     }
-
-    let max = Math.max(...clusterPoints.map(d => d.count))
-
-    let radScale = d3.scale.sqrt()
-      .domain([0, max])
-      .range([1, (clusterRange / 2)])
 
     mainGrp.selectAll('circle').remove()
     // add circles on top of the map
     let circs = mainGrp.selectAll('circle')
-      .data(clusterPoints)
+      .data(nodes)
       .enter()
         .append('circle')
           .attr('class', 'circClass')
           // set radius depending on your data (e.g. estimated population)
-          .attr('r', d => radScale(d.count))
-          .attr('stroke-width', `${1/Math.sqrt(config.scale)}px`)
-          .attr('transform', function(d) {
-            if (isNaN(d.x) || isNaN(d.y)) {
-              return `translate(-9999, -9999)` // move these off the screen
-            }
-            else {
-              return `translate(${d.x}, ${d.y})`
-            }
+          .attr({
+            r: d => d.r,
+           cx: d => d.x || -9999,
+           cy: d => d.y || -9999,
+          fill: function(d) {
+            let col = 'green'
+            if (d.count > 3) col = 'blue'
+            if (d.count > 5) col = 'red'
+            return col
+          },
+          stroke: 'black',
+          'stroke-width': `${0.5/Math.sqrt(config.scale)}px`,
           })
-          .style({ fill: 'red', stroke: 'white', opacity: 0.67 });
+          .style('opacity', 0.7)
+
+    circs.on('click', function(d, i) {
+      console.log('count', this.__data__.count)
+      console.log('index', this.__data__.index)
+      console.log('data', this.__data__)
+    })
+
+
+    let label = mainGrp.append('g')
+    mainGrp.selectAll('text').remove()
+    label.selectAll('text')
+      .data(nodes)
+      .enter()
+        .append('text')
+        .text(function(d) {
+          if (d.r > 0) return `${d.count}`
+        })
+        .attr('transform', d => `translate(${d.x}, ${d.y})`)
+        .style('font-size', d => `${10 / Math.sqrt(config.scale)}px`)
+  }
+
+
+  // Find the nodes within the specified rectangle.
+  function search(quadtree, cx, cy, ra) {
+    quadtree.visit(function(quad, x0, y0, x1, y1) {
+      let n2 = quad.point,
+          r  = ra;
+
+      if (n2) {
+        if (n2.index > n1.index && n1.a && n2.a) {
+          let x = cx - n2.x,
+              y = cy - n2.y,
+              r = n2.r + ra,
+              a, b;
+
+          if (x * x + y * y < r * r) {
+
+            n1.collided = true
+            n2.collided = true
+
+            if (n2.a > n1.a) a = n2, b = n1;
+            else             a = n1, b = n2;
+
+            a.x = (a.x * a.a + b.x * b.a)/(a.a + b.a)
+            a.y = (a.y * a.a + b.y * b.a)/(a.a + b.a)
+
+            a.count++
+            a.a += b.a
+            a.r = Math.sqrt(a.a / pi)
+
+            b.a = 0
+            b.r = 0
+          }
+        }
+
+        return
+
+      }
+
+    return x0 > cx + r || x1 < cx - r || y0 > cy + r || y1 < cy - r
+
+    });
   }
 
   function panAndZoom() {
@@ -157,26 +226,10 @@ function draw_map(config) {
     mainGrp.attr('transform', `translate(${t})scale(${s})`);
     bounds.style('stroke-width', `${1/Math.sqrt(s)}px`)
 
-    if (s != config.scale) {
+    if (config.scale != s) {
       config.scale = s
       update(config)
     }
-  }
-
-  // Find the nodes within the specified rectangle.
-  function search(quadtree, x0, y0, x3, y3) {
-    let validData = [];
-    quadtree.visit(function(node, x1, y1, x2, y2) {
-      let p = node.point;
-      if (p) {
-        p.selected = (p.data.x >= x0) && (p.data.x < x3) && (p.data.y >= y0) && (p.data.y < y3);
-        if (p.selected) {
-          validData.push(p.data);
-        }
-      }
-      return x1 >= x3 || y1 >= y3 || x2 < x0 || y2 < y0;
-    });
-    return validData;
   }
 
   update(config)
